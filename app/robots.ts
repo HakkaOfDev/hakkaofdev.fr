82c9580b178f3110103226206e440f7bd79adb20
HakkaOfDev <frestytv@gmail.com>
2025-03-20 19:37:00 +0100

    feat: optimize SEO

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        crawlDelay: 10,
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    host: process.env.NEXT_PUBLIC_SITE_URL,
+  };
+}
