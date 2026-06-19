8badb3a47e67b78dffbcb1f383265f092a747a47
Alexandre Gossard <frestytv@gmail.com>
2026-06-19 11:42:51 +0200

    chore(main): release 1.13.1

--- lib/constants/site.constants.ts
+++ lib/constants/site.constants.ts
@@ -7,7 +7,7 @@ import {
 } from "@/components/icons/brand";
 
 export const SITE = {
-  version: "1.13.0", // x-release-please-version
+  version: "1.13.1", // x-release-please-version
   url: "https://hakkaofdev.fr",
   repositoryUrl: "https://github.com/hakkaofdev/hakkaofdev.fr",
   title: "Alexandre Gossard | Digital Nomad & Software Engineer",
